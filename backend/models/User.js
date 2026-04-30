731851e36d9ed1bfc3ec50414930e06147796857
diewoloski <diego.abarca.v@mail.pucv.cl>
2026-04-30 13:36:42 -0400

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,10 @@
+javascript
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  name:   { type: String, required: true },
+  pin:    { type: String, default: null }
+});
+
+module.exports = mongoose.model('User', userSchema);
